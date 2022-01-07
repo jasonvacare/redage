6222ba3695cd1f045a517e6981f23820b62c9ce4
Occupant <iaesun@yahoo.com>
2022-01-07 13:26:41 -0500

    First commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const REDAGE = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ REDAGE.abilities = {
+  "str": "REDAGE.AbilityStr",
+  "dex": "REDAGE.AbilityDex",
+  "con": "REDAGE.AbilityCon",
+  "int": "REDAGE.AbilityInt",
+  "wis": "REDAGE.AbilityWis",
+  "cha": "REDAGE.AbilityCha"
+};
+
+REDAGE.abilityAbbreviations = {
+  "str": "REDAGE.AbilityStrAbbr",
+  "dex": "REDAGE.AbilityDexAbbr",
+  "con": "REDAGE.AbilityConAbbr",
+  "int": "REDAGE.AbilityIntAbbr",
+  "wis": "REDAGE.AbilityWisAbbr",
+  "cha": "REDAGE.AbilityChaAbbr"
+};
