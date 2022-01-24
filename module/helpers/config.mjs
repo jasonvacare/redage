645f46d6c21cb87b0c8c667fd72031472ae9f6c3
Occupant <iaesun@yahoo.com>
2022-01-23 23:31:42 -0500

    adding item templates for weapons, armor, etc

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -1,3 +1,49 @@
 export const REDAGE = {};
 
 REDAGE.HeroicLevelThreshold = 10;
+
+REDAGE.ItemLocations = [
+  "Readied",
+  "Stowed",
+  "Camp",
+  "Town"
+];
+
+REDAGE.WeaponProficiencyGroups = [
+  "Brawling",
+  "Exotic",
+  "Great",
+  "Missile",
+  "Pole",
+  "Single",
+  "Thrown"
+];
+
+REDAGE.WeaponAmmunitionType = [
+  "Arrow",
+  "Bolt",
+  "Stone"
+];
+
+REDAGE.ArmorProficiencyGroups = [
+  "Light",
+  "Medium",
+  "Heavy",
+  "Shield"
+];
+
+REDAGE.PanoplyTypes = [
+  "Assistance",
+  "Familiar",
+  "Implement",
+  "Order",
+  "Patron",
+  "PowerFont",
+  "Raimant",
+  "Ritual",
+  "Sacrament",
+  "Sanctity",
+  "Sanctum",
+  "Talisman",
+  "Transfiguration"
+];
